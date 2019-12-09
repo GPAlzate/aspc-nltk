02ef04097a7566f9da8241b363584b981a81c9c8
Ruben Caldera <rpachecocaldera@gmail.com>
2019-12-08 20:40:20 -0800

    f

--- analyzer.py
+++ analyzer.py
@@ -4,14 +4,15 @@ A sentiment analyzer for ASPC course reviews
 # from vader_sentiment.vader_sentiment import *
 from vader_sentiment.vader_sentiment import SentimentIntensityAnalyzer
 import string
+import math
 import re
 analyser = SentimentIntensityAnalyzer()
 
 def sentiment_analyzer_scores(sentence):
     score = analyser.polarity_scores(sentence)
     # return the compound sentiment analysis score
-    return score["compound"]
-    # print("{:-<40} {}".format(sentence, str(score)))
+    return score
+
 
 def analyze_file(inFile, outFile, startIndex):
     """
@@ -26,20 +27,43 @@ def analyze_file(inFile, outFile, startIndex):
     out_file.write("Review Number, Class ID, Comment Sentiment Score, Rating \n")
     # read header
     csv_file.readline()
+    count = 0
 
     for review in csv_file:
 
         review = review.split("\t")
         class_id = review[0]
         comment = review[1]
-        rating = review[5]
+        rating = int(review[5])
         # if they wrote a comment and gave a course review
         if comment != "n/a" and rating != "-1":
             sentiment_score = sentiment_analyzer_scores(comment)
+            max = -1
+            prediction = -1
+            label = -1
+            for polarity in sentiment_score:
+                if sentiment_score[polarity] > max:
+                    max = sentiment_score[polarity]
+                    if polarity == "neg":
+                        prediction = -1
+                    elif polarity == "neu":
+                        prediction = 0
+                    else:
+                        prediction = 1
 
-            out_file.write(str(startIndex) + ", " + class_id + ", " + str(sentiment_score) + ", " + str(rating))
+            if rating < 3:
+                label = -1
+            elif rating == 3:
+                label = 0
+            else:
+                label = 1
+            if label == prediction:
+                count += 1
+
+            # out_file.write(str(startIndex) + ", " + class_id + ", " + str(sentiment_score) + ", " + str(rating) )
 
             startIndex += 1
+    print(count / startIndex)
     out_file.close()
     print("Finished analyzing. Wrote to file")
 
@@ -89,8 +113,8 @@ def addSpace(words):
 
 
 def main():
-    # analyze_file("course_reviews2.csv", "analyzedFile.txt", 1)
-    preProcessFile("course_reviews2.csv", "preprocessed.txt")
+    analyze_file("course_reviews2.csv", "analyzedFile.txt", 1)
+    # preProcessFile("course_reviews2.csv", "preprocessed.txt")
 if __name__ == '__main__':
     main()
     # sample = "(Effort you put in) x100 = What you get out of it don't be afraid of her ; don't be afraid of seeming stupid, she knows the material a heck ton better than you do ; go talk to her after class ; do the work!"
